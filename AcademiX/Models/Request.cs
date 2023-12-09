1efca36002c08bf124a3f86e2de012c3dec85f46
Atanas Adamov <147752297+AtanasAd@users.noreply.github.com>
2023-12-09 16:33:04 +0200

    Add files via upload

---
+++ AcademiX/Models/Request.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AcademiX.Models
+{
+    public enum RequestStatus
+    {
+        Draft,
+        Send,
+        Waiting,
+        Accepted,
+        Rejected
+    }
+
+    public class Request
+    { 
+            public int Id { get; set; }
+            public int StudentId { get; set; }
+            public int DegreeSupervisorId { get; set; }
+            public int DegreeId { get; set; }
+            public RequestStatus Status { get; set; }
+     }
+}
