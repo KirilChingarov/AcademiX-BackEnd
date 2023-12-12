3db3255e481a9537c22dd24d7b046adf2446dc31
Atanas Adamov <147752297+AtanasAd@users.noreply.github.com>
2023-12-12 17:23:34 +0200

    Add files via upload

---
+++ AcademiX/Models/DegreeProject.cs
@@ -0,0 +1,24 @@
+﻿namespace AcademiX.Models
+{
+
+    public class DegreeProject
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public int DegreeSupervisorId { get; set; }
+        public int ReviewerId { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public DegreeStatus Status { get; set; }
+    }
+    public enum DegreeStatus
+    {
+        Draft,
+        InProgress,
+        Review,
+        Defense,
+        Completed
+    }
+
+
+}
