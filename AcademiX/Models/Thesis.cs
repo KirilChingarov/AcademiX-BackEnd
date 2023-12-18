589c59811157771c3dd6b1a018cb80012bf70476
zahari-ninov <133575840+HarkataBG@users.noreply.github.com>
2023-12-18 21:04:07 +0200

    Add thesis model

---
+++ AcademiX/Models/Thesis.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AcademiX.Models
+{
+    public class Thesis
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Title { get; set; }
+
+        [StringLength(500)]
+        public string Description { get; set; }
+      
+        public Thesis(int id)
+        {
+            Id = id;
+            Title = string.Empty;
+            Description = string.Empty;
+        }
+
+        public Thesis(int id, string name, string description = "") : this(id)
+        {
+            Title = name;
+            Description = description;
+        }
+    }
+}
